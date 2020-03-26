95bbe75adc064d2039fd5b0452af76287900693d
Michael Pye <mail@mdpye.co.uk>
2020-03-26 11:56:01 +0000

    Accept master key as base64

---
+++ tests/config.php
@@ -0,0 +1,6 @@
+<?php
+
+define('PUSHERAPP_APPID', '58322');
+define('PUSHERAPP_AUTHKEY', 'a26a08df69b43de776d8');
+define('PUSHERAPP_SECRET', '7122c554d686b72ba6e7');
+define('PUSHERAPP_HOST', 'http://api.pusherapp.com');
